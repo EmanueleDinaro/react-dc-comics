05311dd88f187bff837c6ded6bb5c4eab0597fe1
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-28 19:24:37 +0100

    feat: add footer mid

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,85 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-top container">
-        <ul>
-          <li>
-            <img src="/img/buy-comics-digital-comics.png" alt="" />
-            <a href="#">digital comics</a>
-          </li>
-          <li>
-            <img src="/img/buy-comics-merchandise.png" alt="" />
-            <a href="#">dc merchandise</a>
-          </li>
-          <li>
-            <img src="/img/buy-comics-shop-locator.png" alt="" />
-            <a href="#">subscription</a>
-          </li>
-          <li>
-            <img src="/img/buy-comics-subscriptions.png" alt="" />
-            <a href="#">comic shop locator</a>
-          </li>
-          <li>
-            <img src="/img/buy-dc-power-visa.svg" alt="" />
-            <a href="#">dc power visa</a>
-          </li>
-        </ul>
+      <div className="footer-top">
+        <div className="container">
+          <ul>
+            <li>
+              <img src="/img/buy-comics-digital-comics.png" alt="" />
+              <a href="#">digital comics</a>
+            </li>
+            <li>
+              <img src="/img/buy-comics-merchandise.png" alt="" />
+              <a href="#">dc merchandise</a>
+            </li>
+            <li>
+              <img src="/img/buy-comics-shop-locator.png" alt="" />
+              <a href="#">subscription</a>
+            </li>
+            <li>
+              <img src="/img/buy-comics-subscriptions.png" alt="" />
+              <a href="#">comic shop locator</a>
+            </li>
+            <li>
+              <img src="/img/buy-dc-power-visa.svg" alt="" />
+              <a href="#">dc power visa</a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="footer-mid">
-        <div></div>
+        <div className="container">
+          <div className="link-info">
+            <div className="col">
+              <ul>
+                <h3>dc comics</h3>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+              </ul>
+              <br />
+              <ul>
+                <h3>shop</h3>
+                <li>sample</li>
+                <li>sample</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <h3>dc</h3>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <h3>sites</h3>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+                <li>sample</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="logo-bg"></div>
       </div>
       <div className="footer-bot"></div>
     </footer>
