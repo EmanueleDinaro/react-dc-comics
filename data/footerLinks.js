3e8193e0e7111dd74d4053ee4d9dab573b0a066f
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-29 17:41:53 +0100

    feat: add Footer Links objects array & export

---
+++ data/footerLinks.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    id: 1,
+    category: "dc comics",
+    links: [
+      { id: "1", title: "characters", url: "#" },
+      { id: "2", title: "comics", url: "#" },
+      { id: "3", title: "Movies", url: "#" },
+      { id: "4", title: "tv", url: "#" },
+      { id: "5", title: "games", url: "#" },
+      { id: "6", title: "videos", url: "#" },
+      { id: "7", title: "news", url: "#" },
+    ],
+  },
+  {
+    id: 1,
+    category: "shop",
+    links: [
+      { id: "1", title: "Shop DC", url: "#" },
+      { id: "2", title: "Shop DC collectibles", url: "#" },
+    ],
+  },
+  {
+    id: 1,
+    category: "dc",
+    links: [
+      { id: "1", title: "Terms Of Use", url: "#" },
+      { id: "2", title: "Privacy Policy (New)", url: "#" },
+      { id: "3", title: "Ad Choices", url: "#" },
+      { id: "4", title: "Advertising", url: "#" },
+      { id: "5", title: "Jobs", url: "#" },
+      { id: "6", title: "Subscriptions", url: "#" },
+      { id: "7", title: "Talent Workshops", url: "#" },
+      { id: "8", title: "CPSC Certificates", url: "#" },
+      { id: "9", title: "Ratings", url: "#" },
+      { id: "10", title: "Shop Help", url: "#" },
+      { id: "11", title: "Contact Us", url: "#" },
+    ],
+  },
+  {
+    id: 1,
+    category: "sites",
+    links: [
+      { id: "1", title: "DC", url: "#" },
+      { id: "2", title: "MAD Magazine", url: "#" },
+      { id: "3", title: "DC Kids", url: "#" },
+      { id: "4", title: "DC Universe", url: "#" },
+      { id: "5", title: "DC Power Visa", url: "#" },
+    ],
+  },
+];
--- data/headerNav.js
+++ data/headerNav.js
@@ -1,67 +1,12 @@
 export default [
-  {
-    id: "1",
-    title: "characters",
-    url: "#",
-    currennt: false,
-  },
-
-  {
-    id: "2",
-    title: "comics",
-    url: "#",
-    currennt: true,
-  },
-
-  {
-    id: "3",
-    title: "Movies",
-    url: "#",
-    currennt: false,
-  },
-
-  {
-    id: "4",
-    title: "tv",
-    url: "#",
-    currennt: false,
-  },
-
-  {
-    id: "5",
-    title: "games",
-    url: "#",
-    currennt: false,
-  },
-
-  {
-    id: "6",
-    title: "collectibles",
-    url: "#",
-    currennt: false,
-  },
-  {
-    id: "7",
-    title: "videos",
-    url: "#",
-    currennt: false,
-  },
-  {
-    id: "8",
-    title: "fans",
-    url: "#",
-    currennt: false,
-  },
-  {
-    id: "9",
-    title: "news",
-    url: "#",
-    currennt: false,
-  },
-  {
-    id: "10",
-    title: "shop",
-    url: "#",
-    currennt: false,
-  },
+  { id: "1", title: "characters", url: "#" },
+  { id: "2", title: "comics", url: "#" },
+  { id: "3", title: "Movies", url: "#" },
+  { id: "4", title: "tv", url: "#" },
+  { id: "5", title: "games", url: "#" },
+  { id: "6", title: "collectibles", url: "#" },
+  { id: "7", title: "videos", url: "#" },
+  { id: "8", title: "fans", url: "#" },
+  { id: "9", title: "news", url: "#" },
+  { id: "10", title: "shop", url: "#" },
 ];
